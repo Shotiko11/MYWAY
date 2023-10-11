e82487473a7b5fa4d80c76a80a84ab493966ffab
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-10-11 13:56:28 +0400

    3d figure

--- script.js
+++ script.js
@@ -1,26 +1,69 @@
 const scene = new THREE.Scene();
 
-      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      camera.position.z = 5;
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
-      const renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-      const geometry = new THREE.BoxGeometry(0.7, 0.7, 0.7);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      cube.position.x = 6;
-      cube.position.y = 2.7;
-      scene.add(cube);
+const geometry = new THREE.BoxGeometry(0.7, 0.7, 0.7);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+cube.position.x = 0; 
+cube.position.y = 0;
+scene.add(cube);
 
-      const animate = () => {
-        requestAnimationFrame(animate);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+const minX = -6;
+const maxX = 6;
+const minY = -2;
+const maxY = 3; 
 
-        renderer.render(scene, camera);
-      };
+let isDragging = false;
+let previousMousePosition = { x: 0, y: 0 };
 
-      animate();
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
+
+document.addEventListener('mousedown', (event) => {
+  isDragging = true;
+  previousMousePosition = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+});
+
+document.addEventListener('mouseup', () => {
+  isDragging = false;
+});
+
+document.addEventListener('mousemove', (event) => {
+  if (isDragging) {
+    const deltaMove = {
+      x: event.clientX - previousMousePosition.x,
+      y: event.clientY - previousMousePosition.y,
+    };
+
+    const newX = cube.position.x + deltaMove.x * 0.01;
+    const newY = cube.position.y - deltaMove.y * 0.01;
+
+    if (newX >= minX && newX <= maxX && newY >= minY && newY <= maxY) {
+      cube.position.x = newX;
+      cube.position.y = newY;
+    }
+
+    previousMousePosition = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+  }
+});
