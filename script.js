8c471602b9f199157ccaf3bf8fc32185dd17e967
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-10-13 15:07:16 +0400

    adding slider to the page

---
+++ script.js
@@ -0,0 +1,10 @@
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
