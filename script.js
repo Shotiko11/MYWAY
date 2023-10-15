05e33a54bb06f949f80acac711310d2abb3e1930
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-10-15 16:34:01 +0400

    adding preloader before website is loaded

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ function updateLoadingText() {
   dotCount = (dotCount % 3) + 1 ; 
 }
 
-setInterval(updateLoadingText, 450); 
+setInterval(updateLoadingText, 600); 
+
+
+swiper.on("slideChange", function () {
+  const slides = document.querySelectorAll(".swiper-slide");
+  slides.forEach((slide, index) => {
+    const video = slide.querySelector(".slider-video video");
+    if (index === swiper.realIndex) {
+      video.classList.remove("inactive");
+    } else {
+      video.classList.add("inactive");
+    }
+  });
+});
