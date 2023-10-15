baab8ae2e3974cae2a000972c2bdc66c7a921ddb
Shotiko11 <shotikokokilashvili9@gmail.com>
2023-10-15 18:11:24 +0400

    adding more beauitful render for the website

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-const loadingText = document.getElementById("loading-text");
-let dotCount = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const loadingText = document.getElementById("loading-text");
+  let dotCount = 0;
 
-function updateLoadingText() {
-  const dots = ".".repeat(dotCount);
-  loadingText.textContent = `Loading${dots}`;
-  dotCount = (dotCount % 3) + 1;
-}
+  function updateLoadingText() {
+    const dots = ".".repeat(dotCount);
+    loadingText.textContent = `Loading${dots}`;
+    dotCount = (dotCount % 3) + 1;
+  }
 
-setInterval(updateLoadingText, 600);
+  setInterval(updateLoadingText, 600);
 
-swiper.on("slideChange", function () {
-  const slides = document.querySelectorAll(".swiper-slide");
-  slides.forEach((slide, index) => {
-    const video = slide.querySelector(".slider-video video");
-    if (index === swiper.realIndex) {
-      video.classList.remove("inactive");
-    } else {
-      video.classList.add("inactive");
-    }
+  swiper.on("slideChange", function () {
+    const slides = document.querySelectorAll(".swiper-slide");
+    slides.forEach((slide, index) => {
+      const video = slide.querySelector(".slider-video video");
+      if (index === swiper.realIndex) {
+        video.classList.remove("inactive");
+      } else {
+        video.classList.add("inactive");
+      }
+    });
   });
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add('show');
+        }, 5000); 
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+  
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
+  
 });
